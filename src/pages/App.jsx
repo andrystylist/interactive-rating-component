e7bb23766a095697673c7199c4a9a736198a224a
Andry <andry.carolina88@gmail.com>
2023-07-04 10:00:38 -0500

    solution of the exercise interactive-rating-component

---
+++ src/pages/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from '../components/Main/Main';
+import Card from '../components/Card/Card';
+import './App.scss';
+
+function App() {
+  return (
+    <Main>
+      <Card />
+    </Main>
+  );
+}
+
+export default App;
